bf673fd525bf89d824864089b0031fd3c50775c9
gvankeerberghen <guillaume.vankeerberghen@realimpactanalytics.com>
2016-12-25 12:48:39 +0100

    MOD for image

--- HelloWorld/index.ios.js
+++ HelloWorld/index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, Image } from 'react-native';
 
 class HelloWorld extends Component {
   render() {
@@ -9,4 +9,17 @@ class HelloWorld extends Component {
   }
 }
 
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('HelloWorld', () => Bananas);
+
+// AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
